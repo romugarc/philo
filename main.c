56e0d4758ab157b13739d88a83b678ef8bca5212
romugarc <romualdgarcia4@gmail.com>
2022-10-07 04:44:34 +0200

    mutex, threads, philos, basic actions

--- main.c
+++ main.c
@@ -16,8 +16,10 @@ int	parse_error(int argc, char **argv)
 		j = 0;
 		while (argv[i][j] != '\0')
 		{
-			if (argv[i][j] <= '0' || argv[i][j] >= '9')
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
 				return (1);
+			}
 			j++;
 		}
 		i++;
@@ -44,15 +46,151 @@ int parse_args(int nb_arg, char** argv, t_arguments *args)
 	return (0);
 }
 
+void	take_fork(t_philo *args_philo)
+{
+	pthread_mutex_lock(args_philo->own_fork);
+	pthread_mutex_lock(args_philo->right_fork);
+	printf("%d has taken a fork\n", args_philo->philo_seat);
+}
+
+void	eating(t_philo *args_philo)
+{
+	printf("%d is eating\n", args_philo->philo_seat);
+	args_philo->nb_eat += 1;
+	usleep(args_philo->time_eat * 1000);
+	pthread_mutex_unlock(args_philo->own_fork);
+	pthread_mutex_unlock(args_philo->right_fork);
+}
+
+void	sleeping(t_philo *args_philo)
+{
+	printf("%d is sleeping\n", args_philo->philo_seat);
+	usleep(args_philo->time_sleep * 1000);
+}
+
+void	thinking(t_philo *args_philo)
+{
+	printf("%d is thinking\n", args_philo->philo_seat);
+}
+
+void	starved(t_philo *args_philo)
+{
+	printf("%d died\n", args_philo->philo_seat);
+}
+
+void	*start_routine(void *arg)
+{
+	t_philo *a;
+	int		b;
+
+	a = (t_philo *)a;
+	b = a->time_die;
+	//take_fork(a);
+	/*eating((t_philo *)arg);
+	sleeping((t_philo *)arg);
+	thinking((t_philo *)arg);
+	*/printf("s");
+	printf("%d\n", b);
+}
+
+void	create_mutex(t_arguments *args)
+{
+	pthread_mutex_t *mutex;
+	int				i;
+
+	mutex = malloc(sizeof(pthread_mutex_t) * args->nb_philo);
+	if (!mutex)
+		return ;
+	i = 0;
+	while (i < args->nb_philo)
+	{
+		pthread_mutex_init(&mutex[i], NULL);
+		i++;
+	}
+	args->mutexes = mutex;
+}
+
+void	create_philos(t_arguments *args)
+{
+	t_philo *philos;
+	int		i;
+
+	philos = malloc(sizeof(t_philo) * args->nb_philo);
+	if (!philos)
+		return ;
+	i = 0;
+	while (i < args->nb_philo)
+	{
+		philos[i].nb_philo = args->nb_philo;
+		philos[i].time_die = args->time_die;
+		philos[i].time_eat = args->time_eat;
+		philos[i].time_sleep = args->time_sleep;
+		philos[i].nb_eat = args->nb_eat;
+		philos[i].philo_seat = i;
+		philos[i].own_fork = &args->mutexes[i];
+		if (i != 0)
+			philos[i].right_fork = &args->mutexes[i - 1];
+		else
+			philos[i].right_fork = &args->mutexes[args->nb_philo];
+		i++;
+	}
+	args->philos = philos;
+}
+
+void	create_threads(t_arguments *args)
+{
+	pthread_t 	*thread;
+	int			i;
+
+	thread = malloc(sizeof(pthread_t) * args->nb_philo);
+	if (!thread)
+		return ;
+	i = 0;
+	while (i < args->nb_philo)
+	{
+		printf("%d\n", args->philos[i].philo_seat);
+		pthread_create(&thread[i], NULL, start_routine, (void *)&args->philos[i]);
+		i++;
+	}
+	args->threads = thread;
+}
+
 int	main(int argc, char **argv)
 {
-	int	nb_arg;
-	t_arguments *args;
+	int			nb_arg;
+	int			i;
+	t_arguments args;
+	t_philo *sauce;
 
 	if (parse_error(argc, argv) == 1)
 		return (0);
 	nb_arg = argc - 1;
 	if (parse_args(nb_arg, argv, &args) == 1)
 		return (0);
+	create_mutex(&args);
+	create_philos(&args);
+	i = 0;
+	while (i < args.nb_philo)
+	{
+		printf("%d\n", args.philos[i].philo_seat);
+		i++;
+	}
+	create_threads(&args);
+	i = 0;
+	while (i < args.nb_philo)
+	{
+		pthread_join(args.threads[i], NULL);
+		i++;
+	}
+	i = 0;
+	while (i < args.nb_philo)
+	{
+		pthread_mutex_unlock(&args.mutexes[i]);
+		pthread_mutex_destroy(&args.mutexes[i]);
+		i++;
+	}
+	free(args.philos);
+	free(args.mutexes);
+	free(args.threads);
 	return (0);
 }
--- philo.h
+++ philo.h
@@ -2,6 +2,19 @@
 # define PHILO_H
 # include <stdlib.h>
 # include <unistd.h>
+# include <pthread.h>
+
+typedef struct s_philo
+{
+    int nb_philo;
+    int time_die;
+    int time_eat;
+    int time_sleep;
+    int nb_eat;
+    int philo_seat;
+    pthread_mutex_t *own_fork;
+    pthread_mutex_t *right_fork;
+}   t_philo;
 
 typedef struct s_arguments
 {
@@ -10,6 +23,9 @@ typedef struct s_arguments
     int time_eat;
     int time_sleep;
     int nb_eat;
+    pthread_mutex_t *mutexes;
+    pthread_t       *threads;
+    t_philo         *philos;
 }   t_arguments;
 
 int	ft_atoi(const char *str);
