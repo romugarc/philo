d08455e630a4ab76a2c32bbfd5193dc9d8a8fdf5
Romuald Garcia <rgarcia@z4r12p1.42lyon.fr>
2022-10-17 17:28:25 +0200

    norme

--- philo/check_deaths.c
+++ philo/check_deaths.c
@@ -6,7 +6,7 @@
 /*   By: rgarcia <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 16:13:59 by rgarcia           #+#    #+#             */
-/*   Updated: 2022/10/17 16:14:13 by rgarcia          ###   ########lyon.fr   */
+/*   Updated: 2022/10/17 17:26:18 by rgarcia          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,23 @@
 
 void	starved(t_philo *dead_philo)
 {
-	struct timeval tv;
+	struct timeval	tv;
 
 	gettimeofday(&tv, NULL);
 	pthread_mutex_lock(&dead_philo->updating);
-	printf("%ld %d died\n", (1000 * tv.tv_sec + tv.tv_usec / 1000) - dead_philo->zero_time, dead_philo->philo_seat);
+	printf("%ld %d died\n", (1000 * tv.tv_sec + tv.tv_usec / 1000) - \
+		dead_philo->zero_time, dead_philo->philo_seat);
 	pthread_mutex_unlock(&dead_philo->updating);
 }
 
 int	check_time_death(t_philo *philo)
 {
-	struct timeval tv;
+	struct timeval	tv;
 
 	gettimeofday(&tv, NULL);
 	pthread_mutex_lock(&philo->updating);
-	if ((1000 * tv.tv_sec + tv.tv_usec / 1000) - philo->last_update > philo->time_die)
+	if ((1000 * tv.tv_sec + tv.tv_usec / 1000) - \
+		philo->last_update > philo->time_die)
 	{
 		pthread_mutex_unlock(&philo->updating);
 		return (1);
@@ -51,8 +53,8 @@ int	count_eat(t_arguments *args)
 	while (i < args->nb_philo)
 	{
 		pthread_mutex_lock(&args->philos[i].updating);
-			if (args->philos[i].nb_eat == 0)
-				j = j + 1;
+		if (args->philos[i].nb_eat == 0)
+			j = j + 1;
 		pthread_mutex_unlock(&args->philos[i].updating);
 		i++;
 	}
@@ -66,8 +68,8 @@ int	check_deaths(t_arguments *args)
 	int	i;
 	int	j;
 
-	i = 0;
-	while (i < args->nb_philo)
+	i = -1;
+	while (i++ < args->nb_philo - 1)
 	{
 		if (args->nb_eat > 0)
 		{
@@ -76,18 +78,16 @@ int	check_deaths(t_arguments *args)
 		}
 		if (check_time_death(&args->philos[i]) == 1)
 		{
-			j = 0;
-			while (j < args->nb_philo)
+			j = -1;
+			while (j++ < args->nb_philo - 1)
 			{
 				pthread_mutex_lock(&args->philos[j].updating);
 				args->philos[j].is_dead = 1;
 				pthread_mutex_unlock(&args->philos[j].updating);
-				j++;
 			}
 			starved(&args->philos[i]);
 			return (1);
 		}
-		i++;
 	}
 	return (0);
 }
